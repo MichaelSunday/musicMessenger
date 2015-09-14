f3be860b9455190803beefe6869bd17c4a8dbc1b
Michael Domingo <MichaelDomingo@Michaels-MacBook-Air.local>
2015-09-14 09:45:52 -0700

    relationships

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,5 @@
-class Genre < ActiveRecord::Base
+class Genre < ActiveRecord::Base		
   belongs_to :user
+  has_many :user_chatroom
+  has_many :user, through: :user_chatroom
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
+	has_many :user_chatroom
+	has_many :genre, through: :user_chatroom
+
+	has_secure_password
+end
+
 	# validates :name, :email, presence: true
 	# validates :email, uniqueness: true
 	# validates :password_digest
-	has_secure_password
-end
