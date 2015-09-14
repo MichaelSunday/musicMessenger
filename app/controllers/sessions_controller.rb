800f04ca8c311c6674b5ba4ebb25c2f9c12e0029
Michael Domingo <MichaelDomingo@Michaels-MacBook-Air.local>
2015-09-13 17:26:07 -0700

    sessionsNewController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,17 @@
 class SessionsController < ApplicationController
-  def new
-  end
+	
+	def new 
+		@user = User.new
+	end
 
-  def create
-  end
+	def create
+		user = User.find_by_email(params[:user][:email]).try(:authenticate, params[:user][:password])
+		if user
+			session[:user_id] = user.id
+			redirect_to messages_path
+		else
+			flash[:error] = 'not logged in'
+		   redirect_to new_session_path
+		end   	
+	end
 end
