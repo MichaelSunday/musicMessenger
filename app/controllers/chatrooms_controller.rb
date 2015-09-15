511247c36befd428fab30893c3456e9852436988
Michael Domingo <MichaelDomingo@Michaels-MacBook-Air.local>
2015-09-15 11:02:50 -0700

    chatboxFail

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,12 +1,13 @@
 class ChatroomsController < ApplicationController
   def index
   	@chatrooms = Chatroom.all
+  	@messages = @chatroom.messages
   end
 
   def show
   	@chatroom = Chatroom.find(params[:id])
   	@messages = Message.where(chatroom_id: @chatroom.id)
-  	
+
 
   end
 
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,12 @@
 class MessagesController < ApplicationController
-  def index
-  end
 
   def new
+    @message = @chatroom.messages.new
   end
 
   def create
+    @message = @chatroom.messages.new(message_params)
+    if @message.save
+      redirect_to chatroom_path(@chatroom)
+    end
   end
-end
