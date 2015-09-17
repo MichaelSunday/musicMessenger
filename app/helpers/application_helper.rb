e16e3db0f4739db5cdb995cae87d3021dd55c48c
Michael Domingo <MichaelDomingo@Michaels-MacBook-Air.local>
2015-09-17 09:33:41 -0700

    EmbedYoutube

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,15 @@ module ApplicationHelper
   	else
   		(text)
   	end
+
+  	if text.include? ("youtube.com")
+  		"YOUU"
+  	elsif text.include? ("soundcloud.com")
+  		"MIXTAPE FIRE"
+  	end
+
+
+
     # youtube_id = youtube_url.split("=").last
     # content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}")
 
